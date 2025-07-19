a5264f96ecac431af812baf0243257cf8419a4b9
Emanuele Tamborrino <temanuele27@gmail.com>
2025-07-19 15:20:24 +0200

    Another fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,14 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
-    server: {
-      fs: {
-        allow: ['..']
-      }
-    }
-  },
   site: 'https://manueee13.github.io',
-  base: '/manueee13.github.io/',
+  base: '/',
   output: 'static',
   build: {
     assets: 'assets'
+  },
+  trailingSlash: 'ignore',
+  vite: {
+    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg']
   }
 });
